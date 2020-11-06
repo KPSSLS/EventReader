4dc1bfaf9125209b5bb6a294e30e9655390237ad
Opilane TTHK <opilane@tthk.ee>
2020-11-06 13:40:12 +0200

    ColorJS

--- javascript/javascripts.js
+++ javascript/javascripts.js
@@ -1,5 +1,8 @@
 const images = ["img/1.jpg", "img/2.jpg", "img/3.jpg", "img/4.jpg"];
 const imageToChange = document.getElementById("image");
+const documentBody = document.getElementsByTagName('body') [0];
+const switchButton = document.getElementById('switch');
+
 
 
 function changeImage(){
@@ -7,4 +10,16 @@ function changeImage(){
     let rnd = Math.floor((Math.random() * images.length-1) + 1);
     console.log(rnd);
     imageToChange.src = images[rnd];
+}
+
+
+function changeMode() {
+    if(switchButton.innerHTML === 'Night'){
+        documentBody.style.background = 'black';
+        switchButton.innerHTML = 'Day';
+    }else {
+        documentBody.style.background = 'white';
+        switchButton.innerHTML = 'Night';
+    }
+
 }
